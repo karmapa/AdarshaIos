d2b63d129e90b8fc6999491c55c35793e0a4eb0f
kmsheng <kmsh3ng@gmail.com>
2015-09-15 14:31:10 +0800

    update StyleSheet of AdarshaIos

--- index.ios.js
+++ index.ios.js
@@ -41,21 +41,44 @@ var AdarshaIos = React.createClass({
 
 var styles = StyleSheet.create({
   container: {
+    padding: 30,
+    marginTop: 5,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 24,
+    margin: 5,
+    backgroundColor:'#FFFFFF',
+    color: '#656565'
+  },
+  searchInput: {
+    height: 36,
+    padding: 10,
+    marginRight: 5,
+    flex: 3,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#a4a4a4',
+    borderRadius: 6,
+    color: '#656565'
+  },
+  button: {
+    height: 36,
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#bb5146',
+    borderColor: '#bb5146',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 2,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    alignSelf: 'stretch'
+  }
 });
 
 AppRegistry.registerComponent('AdarshaIos', () => AdarshaIos);
