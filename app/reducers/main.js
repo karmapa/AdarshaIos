16ac142a1fe9c39a5ec9b4078218a6f6fcd070bc
kmsheng <kmsh3ng@gmail.com>
2015-09-17 02:25:38 +0800

    add reducer main

---
+++ app/reducers/main.js
@@ -0,0 +1,16 @@
+
+const initialState = {
+  count: 0
+};
+
+export default function counter(state = initialState, action = {}) {
+  switch (action.type) {
+    case 'test':
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    default:
+      return state;
+  }
+}
