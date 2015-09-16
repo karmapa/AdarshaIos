a86d40e29948f80fa3f455d4aa2c500e7e4c21a0
kmsheng <kmsh3ng@gmail.com>
2015-09-17 02:26:50 +0800

    add file mainApp.style.js

---
+++ app/containers/mainApp.style.js
@@ -0,0 +1,15 @@
+
+var { StyleSheet } = require('react-native');
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  topContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
+});
