5a59c0eb4ce84aca467a745b0afa3984f7b70153
kmsheng <kmsh3ng@gmail.com>
2015-09-18 01:04:40 +0800

    add file masterView.js

---
+++ app/containers/masterView.js
@@ -0,0 +1,58 @@
+'use strict';
+
+import React, { Component, ScrollView, View, Text, PropTypes } from 'react-native';
+
+import { TabBarIOS, Spinner, Icon} from 'react-native-icons';
+import { styles, stylesTabBar } from './masterView.style';
+import * as mainActions from '../actions/mainActions';
+import { KeyboardSearchView } from '../components';
+
+class MasterView extends Component {
+
+  static PropTypes = {
+    settings: PropTypes.object.isRequired,
+    setSelectedTab: PropTypes.func.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  onTabPress(selectedTab) {
+    this.props.setSelectedTab(selectedTab);
+  }
+
+  render() {
+
+    let {tintColor, barTintColor} = stylesTabBar;
+    let tabBarProps = {tintColor, barTintColor};
+    let {selectedTab} = this.props.settings;
+
+    return (
+      <TabBarIOS {...tabBarProps}>
+        <TabBarIOS.Item title={'Category'} iconName={'ion|ios-book-outline'} iconSize={32}
+            selected={'category' === selectedTab} onPress={this.onTabPress.bind(this, 'category')}>
+          <ScrollView contentContainerStyle={styles.container}>
+            <View style={styles.topContainer}>
+              <Text>One</Text>
+            </View>
+          </ScrollView>
+        </TabBarIOS.Item>
+        <TabBarIOS.Item title={'Keyboard Search'} iconName={'ion|ios-search'} iconSize={32}
+            selected={'keyboardSearch' === selectedTab} onPress={this.onTabPress.bind(this, 'keyboardSearch')}>
+          <KeyboardSearchView />
+        </TabBarIOS.Item>
+        <TabBarIOS.Item title={'Advanced Search'} iconName={'ion|social-buffer'} iconSize={32}
+            selected={'advancedSearch' === selectedTab} onPress={this.onTabPress.bind(this, 'advancedSearch')}>
+          <ScrollView contentContainerStyle={styles.container}>
+            <View style={styles.topContainer}>
+              <Text>Two</Text>
+            </View>
+          </ScrollView>
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+}
+
+export default MasterView;
