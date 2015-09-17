ce1388d42ce71ee1289cc39a752567f48fb70bd4
kmsheng <kmsh3ng@gmail.com>
2015-09-17 20:27:21 +0800

    import KeyboardSearchView

--- app/containers/mainApp.js
+++ app/containers/mainApp.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux/native';
 import { TabBarIOS, Spinner, Icon} from 'react-native-icons';
 import { styles, stylesTabBar } from './mainApp.style';
 import * as mainActions from '../actions/mainActions';
+import { KeyboardSearchView } from '../components';
 
 @connect(state => ({
   state: state.main
@@ -43,11 +44,7 @@ class MainApp extends Component {
         </TabBarIOS.Item>
         <TabBarIOS.Item title={'Keyboard Search'} iconName={'ion|ios-search'} iconSize={32}
             selected={'keyboardSearch' === selectedTab} onPress={this.onTabPress.bind(this, 'keyboardSearch')}>
-          <ScrollView contentContainerStyle={styles.container}>
-            <View style={styles.topContainer}>
-              <Text>Two</Text>
-            </View>
-          </ScrollView>
+          <KeyboardSearchView />
         </TabBarIOS.Item>
         <TabBarIOS.Item title={'Advanced Search'} iconName={'ion|social-buffer'} iconSize={32}
             selected={'advancedSearch' === selectedTab} onPress={this.onTabPress.bind(this, 'advancedSearch')}>
