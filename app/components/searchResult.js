cb0a61faf9fc9f757d44b0a3eb903d43a6ee15aa
kmsheng <kmsh3ng@gmail.com>
2015-09-17 20:25:59 +0800

    add file searchResult.js

---
+++ app/components/searchResult.js
@@ -0,0 +1,39 @@
+
+import React, { Component, PropTypes, View, Text, ScrollView } from 'react-native';
+
+import { styles } from './searchResult.style';
+
+class SearchResult extends Component {
+
+  static defaultProps = {
+    excerpts: []
+  };
+
+  renderItem(item) {
+    return <Text style={styles.text} children={this.highlight(item.text, item.realHits)} />;
+  }
+
+  render() {
+    let {excerpts} = this.props;
+    return <ScrollView>{excerpts.map(this.renderItem)}</ScrollView>;
+  }
+
+  highlight(text, hits) {
+    let ex = 0;
+    let out = [];
+
+    for (let i = 0; i < hits.length; i++) {
+      let now = hits[i][0];
+      if (now > ex) {
+        out.push(<Text key={ex}>{text.substring(ex, now)}</Text>);
+      }
+      out.push(<Text key={'h'+ex} style={styles.highlight}>{text.substr(now, hits[i][1])}</Text>);
+      ex = now += hits[i][1];
+    }
+    out.push(<Text key={ex}>{text.substr(ex)}</Text>);
+
+    return out;
+  }
+}
+
+export default SearchResult;
