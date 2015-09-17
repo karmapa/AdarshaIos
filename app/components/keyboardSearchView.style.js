31014177b2b4cf99edce8add706a9d16fbdb54e0
kmsheng <kmsh3ng@gmail.com>
2015-09-17 20:26:51 +0800

    add file keyboardSearchView.style.js

---
+++ app/components/keyboardSearchView.style.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  view: {
+    paddingTop: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10
+  },
+  searchInput: {
+    borderColor: '#cccccc',
+    borderRadius: 4,
+    borderWidth: 1,
+    color: '#555555',
+    flex: 3,
+    fontSize: 18,
+    height: 36,
+    marginRight: 5,
+    padding: 10
+  }
+});
