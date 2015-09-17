ff92b856c1d34274ad55577d888b7c19fbe727a0
kmsheng <kmsh3ng@gmail.com>
2015-09-18 01:08:38 +0800

    import kfs

--- app/actions/mainActions.js
+++ app/actions/mainActions.js
@@ -1,3 +1,13 @@
+
+import { KsanaFileSystem as kfs } from 'NativeModules';
+
+// shouldn't bind kfs to global
+global.kfs = kfs;
+
+if (! kfs) {
+  throw 'Ksana file system not found. Have you imported ksana-react-native in xcode ?';
+}
+
 export const SET_SELECTED_TAB = 'SET_SELECTED_TAB';
 
 export function setSelectedTab(selectedTab) {
