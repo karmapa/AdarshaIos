ff0467f6384f27b4bb7eb6253f3ed41154a42b98
kmsheng <kmsh3ng@gmail.com>
2015-09-18 16:06:54 +0800

    add prop viewSpinner

--- app/containers/mainApp.style.js
+++ app/containers/mainApp.style.js
@@ -2,6 +2,11 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  viewSpinner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   navigatorIos: {
     flex: 1
   }
