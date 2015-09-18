c7f268a4d24368cb4311189cfd5f1ecaa6fb2493
kmsheng <kmsh3ng@gmail.com>
2015-09-18 16:07:48 +0800

    remove un-used styles

--- app/components/searchResult.style.js
+++ app/components/searchResult.style.js
@@ -2,10 +2,8 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   text: {
-    backgroundColor:'#FFFFFF',
     color: '#656565',
     fontSize: 20,
-    margin: 5
   },
   highlight: {
     color:"#FF7F7F"
