f4c5fb5fce46e8692eb5cd7183fb8b2ad9682bbd
kmsheng <kmsh3ng@gmail.com>
2015-09-19 02:49:14 +0800

    export CategoryView

--- app/components/index.js
+++ app/components/index.js
@@ -1,8 +1,10 @@
 
+import CategoryView from './CategoryView';
 import KeyboardSearchView from './keyboardSearchView';
 import SearchResult from './searchResult';
 
 export {
+  CategoryView,
   KeyboardSearchView,
   SearchResult
 };
