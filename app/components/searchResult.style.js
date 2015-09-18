d3cb9bd0605a06e98c14b93bfa849013f096ff75
kmsheng <kmsh3ng@gmail.com>
2015-09-18 16:08:00 +0800

    change fontSize from 20 -> 16

--- app/components/searchResult.style.js
+++ app/components/searchResult.style.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   text: {
     color: '#656565',
-    fontSize: 20,
+    fontSize: 16,
   },
   highlight: {
     color:"#FF7F7F"
