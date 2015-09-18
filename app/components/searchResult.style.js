838fdfd526f3246d2455755a18624cb277102ee5
kmsheng <kmsh3ng@gmail.com>
2015-09-18 14:57:58 +0800

    remove un-used styles

--- app/components/searchResult.style.js
+++ app/components/searchResult.style.js
@@ -1,21 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    marginTop: 5,
-    padding: 30
-  },
   text: {
     backgroundColor:'#FFFFFF',
     color: '#656565',
     fontSize: 20,
     margin: 5
-  },
-  flowRight: {
-    alignItems: 'center',
-    alignSelf: 'stretch',
-    flexDirection: 'row'
   },
   highlight: {
     color:"#FF7F7F"
