1ec778cbb899d6e9c97604f6a5e271f5d700c554
kmsheng <kmsh3ng@gmail.com>
2015-09-18 16:08:23 +0800

    add prop searchResult

--- app/components/searchResult.style.js
+++ app/components/searchResult.style.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  searchResult: {
+    marginTop: 10
+  },
   text: {
     color: '#656565',
     fontSize: 16,
