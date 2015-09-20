9df5ab6c1a06947bfd5058fd562bcbe57c061d44
kmsheng <kmsh3ng@gmail.com>
2015-09-20 17:42:54 +0800

    export AdvancedSearchView

--- app/components/index.js
+++ app/components/index.js
@@ -1,9 +1,11 @@
 
+import AdvancedSearchView from './advancedSearchView';
 import CategoryView from './categoryView';
 import KeyboardSearchView from './keyboardSearchView';
 import SearchResult from './searchResult';
 
 export {
+  AdvancedSearchView,
   CategoryView,
   KeyboardSearchView,
   SearchResult
