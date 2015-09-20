44176e22968afcb123d78503c352aeb01252218a
kmsheng <kmsh3ng@gmail.com>
2015-09-20 17:13:23 +0800

    update component categoryView

--- app/components/categoryView.js
+++ app/components/categoryView.js
@@ -1,8 +1,9 @@
 'use strict';
 
-import React, { Component, PropTypes, View } from 'react-native';
+import React, { Component, ListView, PropTypes, Text, View } from 'react-native';
 
 import kse from 'ksana-search';
+import { styles } from './categoryView.style';
 
 class CategoryView extends Component {
 
@@ -14,10 +15,48 @@ class CategoryView extends Component {
     super(props);
   }
 
-  render() {
+  state = {
+    dataSource: new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2
+    })
+  };
 
+  componentDidMount() {
+    this._rows = [];
+    this.setState({
+      dataSource: this.getDataSource([1, 2, 3])
+    });
+  }
+
+  getDataSource(rows) {
+    this._rows = this._rows.concat(rows);
+    return this.state.dataSource.cloneWithRows(this._rows);
+  }
+
+  renderRow(row) {
     return (
       <View>
+        <Text>{row}</Text>
+      </View>
+    );
+  }
+
+  onEndReached() {
+    console.log('onEndReached');
+  }
+
+  render() {
+
+    let listViewProps = {
+      dataSource: this.state.dataSource,
+      renderRow: this.renderRow.bind(this),
+      onEndReached: this.onEndReached.bind(this)
+    };
+
+    return (
+      <View style={styles.container}>
+        <Text>Test</Text>
+        <ListView {...listViewProps} />
       </View>
     );
   }
