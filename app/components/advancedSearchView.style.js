2a140d6e394b787debff2c96acbd35ef37def32e
kmsheng <kmsh3ng@gmail.com>
2015-09-20 17:41:42 +0800

    add file advancedSearchView.style.js

---
+++ app/components/advancedSearchView.style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+  },
+  input: {
+    borderColor: '#cccccc',
+    borderRadius: 4,
+    borderWidth: 1,
+    color: '#555555',
+    flex: 1,
+    fontSize: 18,
+    height: 36,
+    marginRight: 5,
+    padding: 10
+  }
+});
