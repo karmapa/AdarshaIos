11a37bd7c301af78acdb0707f0fccf46715b36bf
kmsheng <kmsh3ng@gmail.com>
2015-09-25 18:41:50 +0800

    update stylesheet of categoryView.style.js

--- app/components/categoryView.style.js
+++ app/components/categoryView.style.js
@@ -3,5 +3,17 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  rowContainer: {
+    marginBottom: 7,
+    marginTop: 7
+  },
+  rowView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  rowText: {
+    height: 32,
+    paddingLeft: 14
   }
 });
