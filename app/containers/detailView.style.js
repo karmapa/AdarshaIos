23d24059d499059266f3ea5f95849331b9ca5f7a
kmsheng <kmsh3ng@gmail.com>
2015-10-05 20:51:40 +0800

    update style of detailView

--- app/containers/detailView.style.js
+++ app/containers/detailView.style.js
@@ -2,4 +2,40 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  navbar: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 26,
+    paddingBottom: 26,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  backButton: {
+    flex: 1
+  },
+  title: {
+    marginRight: 10
+  },
+  textView: {
+    paddingLeft: 14,
+    paddingRight: 14
+  },
+  boxButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingBottom: 7
+  },
+  buttonImage: {
+    height: 40,
+    width: 40,
+    textAlign: 'center'
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  }
 });
