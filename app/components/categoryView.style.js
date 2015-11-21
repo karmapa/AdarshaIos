2df6440b97dae243d8c1e98a7e928064920d0597
kmsheng <kmsh3ng@gmail.com>
2015-11-21 12:46:08 +0800

    add style for categoryView

--- app/components/categoryView.style.js
+++ app/components/categoryView.style.js
@@ -1,6 +1,16 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  viewSpinner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  stylesSpinner: {
+    width: 24,
+    height: 24,
+    backgroundColor: 'transparent'
+  },
   title: {
     marginRight: 10
   },
