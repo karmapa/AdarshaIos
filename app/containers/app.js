1857faa5c2e1e3e6c819eea24f9e6066915d8774
kmsheng <kmsh3ng@gmail.com>
2015-12-09 18:45:15 +0800

    re-style app.js

--- app/containers/app.js
+++ app/containers/app.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react-native';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { Provider } from 'react-redux/native';
-import thunk from 'redux-thunk';
-
 import * as reducers from '../reducers';
 import MainApp from './mainApp';
+import React, {Component} from 'react-native';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux/native';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
