53f6cc5457a8fe1490f2e3f2b95934264170de7d
kmsheng <kmsh3ng@gmail.com>
2015-12-15 02:13:19 +0800

    re-style

--- app/helpers/index.js
+++ app/helpers/index.js
@@ -1,16 +1,15 @@
-
+import breadcrumb from './breadcrumb';
 import fetch from './fetch';
 import loadNext from './loadNext';
 import loadPrev from './loadPrev';
 import renderSpinner from './renderSpinner';
-import breadcrumb from './breadcrumb';
 import toc from './toc';
 
 export {
   breadcrumb,
   fetch,
-  toc,
   loadNext,
   loadPrev,
-  renderSpinner
+  renderSpinner,
+  toc
 };
