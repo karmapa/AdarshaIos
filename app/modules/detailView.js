eea4dc72fbe8748185caee5123d128c9532dc3af
kmsheng <kmsh3ng@gmail.com>
2015-12-17 15:59:59 +0800

    re-style

--- app/modules/detailView.js
+++ app/modules/detailView.js
@@ -1,50 +1,36 @@
 import Immutable from 'immutable';
 
-const SET_UTI = 'SET_UTI';
-const SET_TOOLBAR_STATUS = 'SET_TOOLBAR_STATUS';
 const SET_FONT_SIZE = 'SET_FONT_SIZE';
 const SET_LINE_HEIGHT = 'SET_LINE_HEIGHT';
+const SET_TOOLBAR_STATUS = 'SET_TOOLBAR_STATUS';
+const SET_UTI = 'SET_UTI';
 const SET_WYLIE_STATUS = 'SET_WYLIE_STATUS';
 
 const initialState = Immutable.Map({
-  uti: '',
-  toolbarOn: true,
   fontSize: 16,
   lineHeight: 2,
+  toolbarOn: true,
+  uti: '',
   wylieOn: false
 });
 
 const actionsMap = {
 
-  [SET_TOOLBAR_STATUS]: (state, action) => state.set('toolbarOn', action.toolbarOn),
-
   [SET_FONT_SIZE]: (state, action) => state.set('fontSize', action.fontSize),
 
   [SET_LINE_HEIGHT]: (state, action) => state.set('lineHeight', action.lineHeight),
 
-  [SET_WYLIE_STATUS]: (state, action) => state.set('wylieOn', action.wylieStatus),
+  [SET_TOOLBAR_STATUS]: (state, action) => state.set('toolbarOn', action.toolbarOn),
 
-  [SET_UTI]: (state, action) => state.set('uti', action.uti)
+  [SET_UTI]: (state, action) => state.set('uti', action.uti),
+
+  [SET_WYLIE_STATUS]: (state, action) => state.set('wylieOn', action.wylieStatus)
 
 };
 
 export default function reducer(state = initialState, action) {
   const reduceFn = actionsMap[action.type];
   return reduceFn ? reduceFn(state, action) : state;
-}
-
-export function setUti(uti) {
-  return {
-    type: SET_UTI,
-    uti
-  };
-}
-
-export function setToolbarStatus(toolbarOn) {
-  return {
-    type: SET_TOOLBAR_STATUS,
-    toolbarOn
-  };
 }
 
 export function setFontSize(fontSize) {
@@ -59,6 +45,20 @@ export function setLineHeight(lineHeight) {
     type: SET_LINE_HEIGHT,
     lineHeight
   }
+}
+
+export function setToolbarStatus(toolbarOn) {
+  return {
+    type: SET_TOOLBAR_STATUS,
+    toolbarOn
+  };
+}
+
+export function setUti(uti) {
+  return {
+    type: SET_UTI,
+    uti
+  };
 }
 
 export function setWylieStatus(wylieStatus) {
