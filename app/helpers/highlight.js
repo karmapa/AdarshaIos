1f29ca2a9a3b9f5b3b45754655fe00f6a1e496ee
kmsheng <kmsh3ng@gmail.com>
2015-12-20 00:57:42 +0800

    re-style

--- app/helpers/highlight.js
+++ app/helpers/highlight.js
@@ -9,16 +9,21 @@ export default function highlight(text, hits = []) {
   }
 
   return hits.reduce((data, hit, index, arr) => {
+
     let {tags, pos} = data;
     let [start, length] = hit;
+
     if (start > pos) {
       tags.push(<Text key={pos}>{text.substring(pos, start)}</Text>);
     }
+
     tags.push(<Text key={'h' + pos} style={styles.highlight}>{text.substr(start, length)}</Text>);
     data.pos = start += length;
+
     if (index === (arr.length - 1)) {
       tags.push(<Text key={data.pos}>{text.substr(data.pos)}</Text>);
     }
+
     return data;
   }, {tags: [], pos: 0}).tags;
 }
