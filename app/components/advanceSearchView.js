d419422049248e42548d3ee2e33860705023e846
kmsheng <kmsh3ng@gmail.com>
2015-12-21 14:50:56 +0800

    fix issue #19

--- app/components/advanceSearchView.js
+++ app/components/advanceSearchView.js
@@ -217,7 +217,7 @@ class AdvanceSearchView extends Component {
             {divisionNames.map((name, index) => <PickerItemIOS key={index + 1} value={index + 1} label={name} />)}
           </PickerIOS>
 
-          {fields.map(row => <TextInput key={row.name} style={styles.input} autoCapitalize="none"
+          {fields.map(row => <TextInput key={row.name} style={styles.input} autoCapitalize="none" returnKeyType="search"
             placeholder={row.placeholder} onChangeText={this.onInputChange.bind(this, row.name)}
             onSubmitEditing={this.handleSubmit} value={this.props.advanceSearchSettings[row.name]} />)}
 
