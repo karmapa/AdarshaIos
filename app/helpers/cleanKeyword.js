1aa325c0a69bca1e7c124518cd5b82e850843c0c
kmsheng <kmsh3ng@gmail.com>
2015-12-22 22:14:40 +0800

    add cleanKeyword.js

---
+++ app/helpers/cleanKeyword.js
@@ -0,0 +1,23 @@
+import wylie from 'tibetan/wylie';
+
+export default function cleanKeyword(keyword) {
+
+  if (keyword.match(/^\d+\.\d+[abcd]$/)) {
+    return keyword;
+  }
+
+  // escape operators
+  keyword = keyword.replace(/\\/g, '\\\\')
+    .replace(/\*/, '**');
+
+  keyword = wylie.fromWylie(keyword);
+  keyword = keyword.replace(/༌༌/g, '*');
+  return removeLoadingEndingSpace(keyword);
+}
+
+function removeLoadingEndingSpace(keyword) {
+  if ((! keyword) || (keyword.length < 2)) {
+    return keyword;
+  }
+  return keyword.replace(/^་/, '').replace(/་$/, '');
+}
