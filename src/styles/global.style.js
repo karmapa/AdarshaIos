38b5fcf99dc3e332756c71e0c25b3df439d007a8
kmsheng <kmsh3ng@gmail.com>
2015-12-25 02:33:21 +0800

    add style in global.style.js

--- src/styles/global.style.js
+++ src/styles/global.style.js
@@ -1,6 +1,23 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
+  backgroundImageContainer: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  cover: {
+    flex: 1,
+    height: null,
+    width: null
+  },
+  transparentContainer: {
+    backgroundColor: 'transparent',
+    flex: 1
+  },
   container: {
     backgroundColor: '#ffffff',
     flex: 1,
@@ -9,7 +26,8 @@ export const styles = StyleSheet.create({
   viewSpinner: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#ffffff'
   },
   navIcon: {
     alignSelf: 'center',
