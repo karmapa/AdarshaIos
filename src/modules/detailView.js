005f5cbfaac393bd652d74381f17d7b2446ed0c5
kmsheng <kmsh3ng@gmail.com>
2015-12-25 03:52:59 +0800

    move menu actions and state that belongs to main duck

--- src/modules/detailView.js
+++ src/modules/detailView.js
@@ -1,31 +1,18 @@
 import Immutable from 'immutable';
 
 const SET_FIRST_SCROLL = 'SET_FIRST_SCROLL';
-const SET_FONT_SIZE = 'SET_FONT_SIZE';
-const SET_LINE_HEIGHT = 'SET_LINE_HEIGHT';
 const SET_TOOLBAR_STATUS = 'SET_TOOLBAR_STATUS';
-const SET_WYLIE_STATUS = 'SET_WYLIE_STATUS';
 
 const initialState = Immutable.Map({
   firstScroll: false,
-  fontSize: 16,
-  lineHeight: 2,
-  toolbarOn: true,
-  wylieOn: false
+  toolbarOn: true
 });
 
 const actionsMap = {
 
   [SET_FIRST_SCROLL]: (state, action) => state.set('firstScroll', action.scrolled),
 
-  [SET_FONT_SIZE]: (state, action) => state.set('fontSize', action.fontSize),
-
-  [SET_LINE_HEIGHT]: (state, action) => state.set('lineHeight', action.lineHeight),
-
-  [SET_TOOLBAR_STATUS]: (state, action) => state.set('toolbarOn', action.toolbarOn),
-
-  [SET_WYLIE_STATUS]: (state, action) => state.set('wylieOn', action.wylieStatus)
-
+  [SET_TOOLBAR_STATUS]: (state, action) => state.set('toolbarOn', action.toolbarOn)
 };
 
 export default function reducer(state = initialState, action) {
@@ -40,73 +27,9 @@ export function setFirstScroll(scrolled) {
   }
 }
 
-export function setFontSize(fontSize) {
-  return {
-    type: SET_FONT_SIZE,
-    fontSize
-  }
-}
-
-export function setLineHeight(lineHeight) {
-  return {
-    type: SET_LINE_HEIGHT,
-    lineHeight
-  }
-}
-
 export function setToolbarStatus(toolbarOn) {
   return {
     type: SET_TOOLBAR_STATUS,
     toolbarOn
-  };
-}
-
-export function setWylieStatus(wylieStatus) {
-  return {
-    type: SET_WYLIE_STATUS,
-    wylieStatus
-  };
-}
-
-export function increaseFontSize() {
-  return (dispatch, getState) => {
-    let newFontSize = getState().detailView.get('fontSize') + 1;
-    if (newFontSize < 30) {
-      dispatch(setFontSize(newFontSize));
-    }
-  };
-}
-
-export function decreaseFontSize() {
-  return (dispatch, getState) => {
-    let newFontSize = getState().detailView.get('fontSize') - 1;
-    if (newFontSize > 0) {
-      dispatch(setFontSize(newFontSize));
-    }
-  };
-}
-
-export function increaseLineHeight() {
-  return (dispatch, getState) => {
-    let newLineHeight = getState().detailView.get('lineHeight') + 1;
-    if (newLineHeight < 30) {
-      dispatch(setLineHeight(newLineHeight));
-    }
-  };
-}
-
-export function decreaseLineHeight() {
-  return (dispatch, getState) => {
-    let newLineHeight = getState().detailView.get('lineHeight') - 1;
-    if (newLineHeight > 0) {
-      dispatch(setLineHeight(newLineHeight));
-    }
-  };
-}
-
-export function toggleWylieStatus() {
-  return (dispatch, getState) => {
-    let status = getState().detailView.get('wylieOn');
-    dispatch(setWylieStatus(! status));
   };
 }
--- src/modules/main.js
+++ src/modules/main.js
@@ -5,17 +5,23 @@ const open = require('../helpers/openDb');
 
 const SET_DB = 'SET_DB';
 const SET_DB_ERROR = 'SET_DB_ERROR';
+const SET_FONT_SIZE = 'SET_FONT_SIZE';
+const SET_LINE_HEIGHT = 'SET_LINE_HEIGHT';
 const SET_LOADING = 'SET_LOADING';
 const SET_SELECTED_TAB = 'SET_SELECTED_TAB';
 const SET_SUTRA_MAP = 'SET_SUTRA_MAP';
 const SET_TOC_ROWS = 'SET_TOC_ROWS';
+const SET_WYLIE_STATUS = 'SET_WYLIE_STATUS';
 
 const initialState = Immutable.Map({
   db: null,
   dbError: null,
+  fontSize: 16,
+  lineHeight: 2,
   loading: false,
   selectedTab: 'category',
-  sutraMap: {}
+  sutraMap: {},
+  wylieOn: false
 });
 
 const actionsMap = {
@@ -24,11 +30,18 @@ const actionsMap = {
 
   [SET_DB_ERROR]: (state, action) => state('dbError', action.err),
 
+  [SET_FONT_SIZE]: (state, action) => state.set('fontSize', action.fontSize),
+
+  [SET_LINE_HEIGHT]: (state, action) => state.set('lineHeight', action.lineHeight),
+
   [SET_LOADING]: (state, action) => state.set('loading', action.loading),
 
   [SET_SELECTED_TAB]: (state, action) => state.set('selectedTab', action.selectedTab),
 
-  [SET_SUTRA_MAP]: (state, action) => state.set('sutraMap', action.sutraMap)
+  [SET_SUTRA_MAP]: (state, action) => state.set('sutraMap', action.sutraMap),
+
+  [SET_WYLIE_STATUS]: (state, action) => state.set('wylieOn', action.wylieStatus)
+
 };
 
 export default function reducer(state = initialState, action) {
@@ -91,5 +104,69 @@ export function setSutraMap(sutraMap) {
   return {
     type: SET_SUTRA_MAP,
     sutraMap
+  };
+}
+
+export function setFontSize(fontSize) {
+  return {
+    type: SET_FONT_SIZE,
+    fontSize
+  }
+}
+
+export function setLineHeight(lineHeight) {
+  return {
+    type: SET_LINE_HEIGHT,
+    lineHeight
+  }
+}
+
+export function setWylieStatus(wylieStatus) {
+  return {
+    type: SET_WYLIE_STATUS,
+    wylieStatus
+  };
+}
+
+export function increaseFontSize() {
+  return (dispatch, getState) => {
+    let newFontSize = getState().main.get('fontSize') + 1;
+    if (newFontSize < 30) {
+      dispatch(setFontSize(newFontSize));
+    }
+  };
+}
+
+export function decreaseFontSize() {
+  return (dispatch, getState) => {
+    let newFontSize = getState().main.get('fontSize') - 1;
+    if (newFontSize > 0) {
+      dispatch(setFontSize(newFontSize));
+    }
+  };
+}
+
+export function increaseLineHeight() {
+  return (dispatch, getState) => {
+    let newLineHeight = getState().main.get('lineHeight') + 1;
+    if (newLineHeight < 30) {
+      dispatch(setLineHeight(newLineHeight));
+    }
+  };
+}
+
+export function decreaseLineHeight() {
+  return (dispatch, getState) => {
+    let newLineHeight = getState().main.get('lineHeight') - 1;
+    if (newLineHeight > 0) {
+      dispatch(setLineHeight(newLineHeight));
+    }
+  };
+}
+
+export function toggleWylieStatus() {
+  return (dispatch, getState) => {
+    let status = getState().main.get('wylieOn');
+    dispatch(setWylieStatus(! status));
   };
 }
