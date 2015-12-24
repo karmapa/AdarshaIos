b3f375c60a1ae6bb6216445546e30af3c9abef70
kmsheng <kmsh3ng@gmail.com>
2015-12-25 02:32:54 +0800

    update render() of MasterView

--- src/components/MasterView.js
+++ src/components/MasterView.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import React, {Component, ScrollView, View, Text, PropTypes} from 'react-native';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 import {AdvanceSearchView, CategoryView, KeyboardSearchView} from '../components';
@@ -7,6 +5,7 @@ import {TabBarIOS, Spinner, Icon} from 'react-native-icons';
 import {connect} from 'react-redux/native';
 import {setSelectedTab} from '../modules/main';
 import {styles, stylesTabBar} from './MasterView.style';
+import {styles as globalStyles} from '../styles/global.style';
 
 @connect(state => ({
   selectedTab: state.main.get('selectedTab'),
@@ -54,20 +53,22 @@ class MasterView extends Component {
     }
 
     return (
-      <TabBarIOS {...stylesTabBar}>
-        <TabBarIOS.Item title={'Category'} iconName={'ion|ios-book-outline'} iconSize={32}
-          selected={'category' === selectedTab} onPress={this.onTabPress.bind(this, 'category')}>
-          <CategoryView navigator={navigator} tocRows={tocRows} title={title} />
-        </TabBarIOS.Item>
-        <TabBarIOS.Item title={'Keyboard Search'} iconName={'ion|ios-search'} iconSize={32}
-          selected={'keyboardSearch' === selectedTab} onPress={this.onTabPress.bind(this, 'keyboardSearch')}>
-          <KeyboardSearchView navigator={navigator} />
-        </TabBarIOS.Item>
-        <TabBarIOS.Item title={'Advance Search'} iconName={'ion|social-buffer'} iconSize={32}
-          selected={'advanceSearch' === selectedTab} onPress={this.onTabPress.bind(this, 'advanceSearch')}>
-          {this.renderAdvance()}
-        </TabBarIOS.Item>
-      </TabBarIOS>
+      <View style={globalStyles.container}>
+        <TabBarIOS {...stylesTabBar}>
+          <TabBarIOS.Item title={'Category'} iconName={'ion|ios-book-outline'} iconSize={32}
+            selected={'category' === selectedTab} onPress={this.onTabPress.bind(this, 'category')}>
+            <CategoryView navigator={navigator} tocRows={tocRows} title={title} />
+          </TabBarIOS.Item>
+          <TabBarIOS.Item title={'Keyboard Search'} iconName={'ion|ios-search'} iconSize={32}
+            selected={'keyboardSearch' === selectedTab} onPress={this.onTabPress.bind(this, 'keyboardSearch')}>
+            <KeyboardSearchView navigator={navigator} />
+          </TabBarIOS.Item>
+          <TabBarIOS.Item title={'Advance Search'} iconName={'ion|social-buffer'} iconSize={32}
+            selected={'advanceSearch' === selectedTab} onPress={this.onTabPress.bind(this, 'advanceSearch')}>
+            {this.renderAdvance()}
+          </TabBarIOS.Item>
+        </TabBarIOS>
+      </View>
     );
   }
 }
