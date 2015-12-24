9a40d91f3d76898674241517a6d9d23790563d9d
kmsheng <kmsh3ng@gmail.com>
2015-12-25 02:32:34 +0800

    remove styles from Main.style.js

--- src/containers/Main.style.js
+++ src/containers/Main.style.js
@@ -4,18 +4,5 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   navigatorIos: {
     flex: 1
-  },
-  backgroundImageContainer: {
-    flex: 1,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0
-  },
-  cover: {
-    flex: 1,
-    height: null,
-    width: null
   }
 });
