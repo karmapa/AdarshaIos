ac3a7a6c971668a0e993c6ba7a26655f9fff2758
kmsheng <kmsh3ng@gmail.com>
2015-12-25 03:29:08 +0800

    update Menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,34 @@
 import React, {Component, View, PropTypes, TouchableHighlight, Image, Text} from 'react-native';
+import {increaseFontSize, decreaseFontSize, increaseLineHeight, decreaseLineHeight, toggleWylieStatus} from '../modules/detailView';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 import {connect} from 'react-redux/native';
 import {styles} from './Menu.style';
 
 const underlayColor = 'rgba(0, 0, 0, 0)';
 
+@connect(state => ({
+  lineHeight: state.detailView.get('lineHeight'),
+  fontSize: state.detailView.get('fontSize'),
+  wylieOn: state.detailView.get('wylieOn')
+}), {
+  decreaseFontSize,
+  decreaseLineHeight,
+  increaseFontSize,
+  increaseLineHeight,
+  toggleWylieStatus
+})
 class Menu extends Component {
+
+  static PropTypes = {
+    decreaseFontSize: PropTypes.func.isRequired,
+    decreaseLineHeight: PropTypes.func.isRequired,
+    fontSize: PropTypes.number.isRequired,
+    increaseFontSize: PropTypes.func.isRequired,
+    increaseLineHeight: PropTypes.func.isRequired,
+    lineHeight: PropTypes.number.isRequired,
+    toggleWylieStatus: PropTypes.func.isRequired,
+    wylieOn: PropTypes.bool.isRequired
+  }
 
   constructor(props) {
     super(props);
@@ -14,37 +37,49 @@ class Menu extends Component {
   shouldComponentUpdate = shouldPureComponentUpdate;
 
   render() {
+
+    let {
+      decreaseFontSize,
+      decreaseLineHeight,
+      fontSize,
+      increaseFontSize,
+      increaseLineHeight,
+      lineHeight,
+      toggleWylieStatus,
+      wylieOn
+    } = this.props;
+
     return (
       <View style={styles.container}>
         <View style={styles.menu}>
 
           <View style={[styles.item, styles.row]}>
-            <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={this.decreaseLineHeight}>
+            <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={decreaseLineHeight}>
               <Image style={styles.buttonImage} source={require('image!icon-line-height-minus')} />
             </TouchableHighlight>
-            <Text style={styles.value}>100</Text>
-            <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={this.increaseLineHeight}>
+            <Text style={styles.value}>{lineHeight}</Text>
+            <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={increaseLineHeight}>
               <Image style={styles.buttonImage} source={require('image!icon-line-height-add')} />
             </TouchableHighlight>
           </View>
 
           <View style={[styles.item, styles.row]}>
-            <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={this.decreaseFontSize}>
+            <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={decreaseFontSize}>
               <Image style={styles.buttonImage} source={require('image!icon-font-size-minus')} />
             </TouchableHighlight>
-            <Text style={styles.value}>100</Text>
-            <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={this.increaseFontSize}>
+            <Text style={styles.value}>{fontSize}</Text>
+            <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={increaseFontSize}>
               <Image style={styles.buttonImage} source={require('image!icon-font-size-add')} />
             </TouchableHighlight>
           </View>
 
           <View style={[styles.item, styles.row]}>
             <View style={styles.wylie}>
-              <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={this.toggleWylieStatus}>
+              <TouchableHighlight underlayColor={underlayColor} style={styles.button} onPress={toggleWylieStatus}>
                 <Image style={styles.buttonImage} source={require('image!icon-tibetan-wylie-switch')} />
               </TouchableHighlight>
             </View>
-            <Text style={styles.value}>True</Text>
+            <Text style={styles.value}>{wylieOn ? 'Wylie' : 'Tibetan'}</Text>
           </View>
 
         </View>
