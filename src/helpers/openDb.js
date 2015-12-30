81870e9573fbca8fbf95e401e7f46f11e83ffd5e
kmsheng <kmsh3ng@gmail.com>
2015-12-30 15:37:50 +0800

    remove kfs

--- src/helpers/openDb.js
+++ src/helpers/openDb.js
@@ -1,13 +1,11 @@
-import {DB_NAME} from '../constants/AppConstants';
-import {KsanaFileSystem as kfs} from 'NativeModules';
-
 import kde from 'ksana-database';
+import {DB_NAME} from '../constants/AppConstants';
 
 export default function openDb(dbName = DB_NAME) {
 
   return new Promise((resolve, reject) => {
 
-    kde.open(dbName, {kfs}, function(err, db) {
+    kde.open(dbName, function(err, db) {
       if (db) {
         resolve(db);
       }
--- src/helpers/toc.js
+++ src/helpers/toc.js
@@ -1,5 +1,4 @@
 import {DB_NAME} from '../constants/AppConstants';
-import {KsanaFileSystem as kfs} from 'NativeModules';
 
 import ksa from 'ksana-simple-api';
 
@@ -7,7 +6,7 @@ export default function toc(options = {}) {
 
   return new Promise((resolve, reject) => {
 
-    options = Object.assign({db: DB_NAME, kfs}, options);
+    options = Object.assign({db: DB_NAME}, options);
 
     ksa.toc(options, (err, res) => {
       if (err) {
