5a475c3778399d22a186d1b93d93200692e0c724
kmsheng <kmsh3ng@gmail.com>
2016-01-06 04:16:30 +0800

    extract keyboardHeight to module

--- src/components/KeyboardSpacer.js
+++ src/components/KeyboardSpacer.js
@@ -1,45 +1,28 @@
 import React, {Animated, Component, DeviceEventEmitter, PropTypes, View, Platform} from 'react-native';
+import {connect} from 'react-redux/native';
 
 const KITKAT = 19;
 
+@connect(state => ({
+  keyboardHeight: state.main.get('keyboardHeight')
+}))
 class KeyboardSpacer extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+  static PropTypes = {
+    keyboardHeight: PropTypes.number.isRequired
+  };
 
   state = {
     keyboardHeightAnim: new Animated.Value(0)
   };
 
-  componentWillMount() {
-    this._registerEvents();
+  componentWillReceiveProps(nextProps) {
+    this.setKeyboardHeight(nextProps.keyboardHeight);
   }
 
-  componentWillUnmount() {
-    this._unRegisterEvents();
-  }
-
-  _registerEvents() {
-    this._keyboardDidShowSubscription = DeviceEventEmitter.addListener('keyboardDidShow', e => this._keyboardDidShow(e));
-    this._keyboardDidHideSubscription = DeviceEventEmitter.addListener('keyboardDidHide', e => this._keyboardDidHide(e));
-  };
-
-  _unRegisterEvents = () => {
-    this._keyboardDidShowSubscription.remove();
-    this._keyboardDidHideSubscription.remove();
-  };
-
-  _keyboardDidShow = e => {
+  setKeyboardHeight = height => {
     Animated.timing(this.state.keyboardHeightAnim, {
-      toValue: e.endCoordinates.height,
-      duration: 80
-    }).start();
-  };
-
-  _keyboardDidHide = () => {
-    Animated.timing(this.state.keyboardHeightAnim, {
-      toValue: 0,
+      toValue: height,
       duration: 80
     }).start();
   };
