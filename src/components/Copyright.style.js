210353f786781f087ed42854659ad40fb435a2f3
kmsheng <kmsh3ng@gmail.com>
2016-01-07 23:45:04 +0800

    add Copyright.style

---
+++ src/components/Copyright.style.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 14
+  },
+  title: {
+    fontSize: 24,
+    paddingTop: 7,
+    paddingBottom: 7
+  },
+  content: {
+    fontSize: 16,
+    lineHeight: 24
+  }
+});
