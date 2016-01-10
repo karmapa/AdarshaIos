1ab69dd3e7477413766381fd8acb5323324c5d41
kmsheng <kmsh3ng@gmail.com>
2016-01-10 21:25:46 +0800

    add queries to fit new version of debugger

--- ios/AdarshaIos/AppDelegate.m
+++ ios/AdarshaIos/AppDelegate.m
@@ -18,7 +18,7 @@
   NSURL *jsCodeLocation;
 
 #if TARGET_IPHONE_SIMULATOR
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 #else
   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
