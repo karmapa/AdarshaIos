41b9362360095c1be2ba32427f5527564898fc05
kmsheng <kmsh3ng@gmail.com>
2016-01-12 22:36:34 +0800

    add Background style

---
+++ src/components/Background.style.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  whiteBackground: {
+    backgroundColor: '#ffffff',
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0
+  }
+});
