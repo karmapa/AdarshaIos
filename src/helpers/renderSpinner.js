a31b56eb74b04851abf3be6530a52f48cb5501fc
kmsheng <kmsh3ng@gmail.com>
2016-01-12 17:20:24 +0800

    add option to renderSpinner

--- src/helpers/renderSpinner.js
+++ src/helpers/renderSpinner.js
@@ -2,7 +2,7 @@ import React, {View} from 'react-native';
 import {Spinner} from 'react-native-icons';
 import {styles as globalStyles} from '../styles/global.style';
 
-export default function renderSpinner() {
+export default function renderSpinner(options = {}) {
 
   let spinnerProps = {
     name: 'ion|load-c',
@@ -11,8 +11,14 @@ export default function renderSpinner() {
     style: globalStyles.spinner
   };
 
+  let viewStyle = [globalStyles.viewSpinner];
+
+  if (options.transparent) {
+    viewStyle.push({backgroundColor: 'transparent'});
+  };
+
   return (
-    <View style={globalStyles.viewSpinner}>
+    <View style={viewStyle}>
       <Spinner {...spinnerProps} />
     </View>
   );
