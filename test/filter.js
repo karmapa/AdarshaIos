dd3c5fe4b28c57326216dfd241c78d0eedce0346
kmsheng <kmsh3ng@gmail.com>
2016-01-27 16:17:36 +0800

    add test files

---
+++ test/filter.js
@@ -0,0 +1,28 @@
+var should = require('should');
+var ksa = require('ksana-simple-api');
+var wylie = require('tibetan/wylie');
+
+describe('ksa.filter', function() {
+
+  it('should have 1.7b for keyword "ga dag"', function(done) {
+
+    ksa.filter({
+      db: 'jiangkangyur',
+     'phrase_sep': '།',
+      q: wylie.fromWylieWithWildcard('ga dag'),
+      field: 'head'
+    }, cb);
+
+    function cb(err, rows) {
+
+      var utis = rows.map(function(row) {
+        return row.uti;
+      });
+
+      utis.should.containDeep(['1.2a', '1.135b', '1.7b']);
+
+      done();
+    }
+  });
+
+});
