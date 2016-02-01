8c156a284dd9fcbdd08516c7b30e83fbf46f102a
kmsheng <kmsh3ng@gmail.com>
2016-02-01 16:34:40 +0800

    add helper fetchBySutraId

---
+++ src/helpers/fetchBySutraId.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+import {fetch, getFieldRange, vposToUti} from '.';
+
+export default function fetchBySutraId(sutraIds) {
+
+  return getFieldRange({
+    field: 'sutra',
+    values: sutraIds
+  })
+  .then(rows => {
+
+    if (_.isEmpty(rows)) {
+      return [];
+    }
+
+    let vposEnds = _.map(rows, row => {
+      return row[1];
+    });
+
+    return vposToUti(vposEnds);
+  })
+  .then(utis => {
+    if (_.isEmpty(utis)) {
+      console.log('here');
+      return [];
+    }
+    return fetch({uti: utis});
+  });
+}
