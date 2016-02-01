d008cbde7db38e1af6cd961d5f846abb53e976e3
kmsheng <kmsh3ng@gmail.com>
2016-02-01 16:36:55 +0800

    sort helpers/index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,25 +4,25 @@ import fetch from './fetch';
 import fetchBySutraId from './fetchBySutraId';
 import filter from './filter';
 import findBiographyBySutraId from './findBiographyBySutraId';
+import getDeviceSize from './getDeviceSize';
 import getFieldRange from './getFieldRange';
 import getHighlightStyle from './getHighlightStyle';
 import getOffset from './getOffset';
 import getOrientation from './getOrientation';
 import getUti from './getUti';
-import getDeviceSize from './getDeviceSize';
 import highlight from './highlight';
+import isCrashStr from './isCrashStr';
+import isPbId from './isPbId';
 import loadNext from './loadNext';
 import loadPrev from './loadPrev';
 import openDb from './openDb';
 import removeLatin from './removeLatin';
 import renderSpinner from './renderSpinner';
+import searchInSutra from './searchInSutra';
 import storage from './storage';
 import toc from './toc';
 import utiToSutraId from './utiToSutraId';
 import vposToSutraId from './vposToSutraId';
-import searchInSutra from './searchInSutra';
-import isCrashStr from './isCrashStr';
-import isPbId from './isPbId';
 import vposToUti from './vposToUti';
 
 export {
@@ -39,17 +39,17 @@ export {
   getOrientation,
   getUti,
   highlight,
+  isCrashStr,
+  isPbId,
   loadNext,
   loadPrev,
   openDb,
   removeLatin,
   renderSpinner,
-  storage,
   searchInSutra,
+  storage,
   toc,
   utiToSutraId,
   vposToSutraId,
-  isCrashStr,
-  vposToUti,
-  isPbId
+  vposToUti
 };
